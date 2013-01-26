a96fc5765d0f9d1ab7dcd27f0072e853b1b64e94
Linden Krouse <ztaticnull@gmail.com>
2013-01-26 19:36:28 +0000

    Added HeartBeets and deleted stuff.

--- HeartFarm/HeartFarm/ScreenManaferAssets/Menu.cs
+++ HeartFarm/HeartFarm/ScreenManaferAssets/Menu.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
-using HeartFarm.ScreenManagerAssets.Menus;
 using System.Diagnostics;
 
 namespace HeartFarm
