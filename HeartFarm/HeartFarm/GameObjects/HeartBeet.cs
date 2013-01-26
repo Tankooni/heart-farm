7f6cce10b04bcd3a752026f9be64126287d97d47
flareforce5 <flareforce5@localhost>
2013-01-26 20:52:27 +0000

    Fixed ScreenManager and screens and such.

--- HeartFarm/HeartFarm/GameObjects/HeartBeet.cs
+++ HeartFarm/HeartFarm/GameObjects/HeartBeet.cs
@@ -1,17 +1,23 @@
 using System;
+using Microsoft.Xna.Framework.Content;
 
 namespace HeartFarm
 {
-	public class HeartBeet : Listener
+	public class HeartBeet : BaseSprite, Listener
 	{
-		public HeartBeet ()
+		public HeartBeet (ContentManager cm)
+			:base(cm, "heart_beet")
 		{
 
 		}
 
-		public void OnEvent(Event e)
+		public void OnEvent (Event e)
 		{
+			if (e is MousePosition) {
+				MousePosition me= (MousePosition) e;
 
+
+			}
 		}
 	}
 }
