f7a1df791087891f9a38d77b9ef937856ffdb179
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2013-01-27 02:16:53 +0000

    Places stuff in the place.

--- HeartFarm/HeartFarm/ScreenManagerAssets/UI.cs
+++ HeartFarm/HeartFarm/ScreenManagerAssets/UI.cs
@@ -12,6 +12,7 @@ namespace HeartFarm
 		public UI(ContentManager Content)
 		{
 			boodles = new BloodGauge(Content);
+			boodles.Position = new Vector(Game1.ScreenSize.X*0.995f - boodles.Width , Game1.ScreenSize.Y*0.495f - boodles.Height/2);
 		}
 
 		public override Screen update(InputManager IM)
