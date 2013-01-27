afcdf3417cb153b70b89da6d6854dd5d5bc25311
Thespian42 <Thespian42@localhost>
2013-01-27 04:31:17 +0000

    Added the Level.cs file, and changed the tiles

---
+++ HeartFarm/HeartFarm/Level.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HeartFarm
+{
+	public class Level:Screen
+	{
+		BaseSprite[,] tileArray;
+
+		public Level ()
+		{
+			tileArray = new BaseSprite[5, 5];
+
+			for (int i = 0; i < 5; i++)
+			{
+				for(int j = 0; j < 5; j++)
+				{
+					//tileArray[i][j] = new BaseSprite(
+				}
+
+			}
+		}
+	}
+}
+
