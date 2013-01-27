93cb774adb3b4901c835bb71729db2b052e859ca
tankooni <tankooni@localhost>
2013-01-27 12:26:44 +0000

    I made something work.

--- HeartFarm/HeartFarm/ScreenManagerAssets/UI.cs
+++ HeartFarm/HeartFarm/ScreenManagerAssets/UI.cs
@@ -5,11 +5,12 @@ using Microsoft.Xna.Framework.Content;
 
 namespace HeartFarm
 {
-	public class UI : Screen
+	public class UI : Screen, Listener
 	{
 		BloodGauge boodles;
 		Clock cüdles;
 		ToolBar toodles;
+		BaseSprite Cursor;
 		
 		public UI(ContentManager Content)
 		{
@@ -20,6 +21,10 @@ namespace HeartFarm
 			cüdles.Position = new Vector(Game1.ScreenSize.X*0.5f, 0);
 
 			toodles = new ToolBar(Content);
+
+			Cursor = new BaseSprite(Content, "Syringe");
+			Cursor.Origin.Y = Cursor.Height;
+			EventManager.g_EM.AddListener(new MousePosition(), this);
 		}
 
 		public override Screen update()
@@ -35,6 +40,17 @@ namespace HeartFarm
 			boodles.Draw(spriteBatch, gameTime);
 			cüdles.Draw(spriteBatch, gameTime);
 			toodles.Draw(spriteBatch, gameTime);
+			Cursor.Draw(gameTime, spriteBatch);
+		}
+
+		public void OnEvent (Event e)
+		{
+			if (e is MousePosition) {
+				MousePosition mp = (MousePosition)e;
+				Cursor.Position.X = mp.pos.X;
+				Cursor.Position.Y = mp.pos.Y;
+				e.isDoneProcessing = true;
+			}
 		}
 	}
 }
---
+++ HeartFarm/HeartFarm/ScreenManagerAssets/UIAssets/Cursor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HeartFarm
+{
+	public class Cursor
+	{
+		public Cursor ()
+		{
+		}
+	}
+}
+
